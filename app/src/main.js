083debc156b43a2d8f794f37e5a6dddf3cc6fd22
Francois Landais <francois.landais@gmail.com>
2026-09-12 15:58:07 +0200

    Revert "Add geographic Explorer layouts"
    This reverts commit 451d53efe666a6617851a6310ecdebdb3e0e87c0.

--- app/src/main.js
+++ app/src/main.js
@@ -17,7 +17,7 @@ import {
   storeFileHandle
 } from "./data/localFileSource.js?v=20260704-local-source";
 import { createDataStore } from "./data/dataStore.js?v=20260806-impossible-combinations";
-import { renderAppState, wireUi } from "./ui/dataScreen.js?v=20260912-geography-layout";
+import { renderAppState, wireUi } from "./ui/dataScreen.js?v=20260912-finrep-labels";
 import {
   buildStandaloneHtml,
   getStandaloneModuleDependencies,
